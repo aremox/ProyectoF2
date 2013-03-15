9ecc84b86671a3a389a14fc1f0602da6d0dd4c89
64500 <64500@hp3031-11.CENTRO.LOCAL>
2013-03-15 18:01:23 +0100

    Subida proyecto base

---
+++ ProyectoCuadroMando/src/ProyectoCuadroMando/ProyectoCuadroMando.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProyectoCuadroMando;
+
+/**
+ *
+ * @author 64500
+ */
+public class ProyectoCuadroMando {
+    
+}
