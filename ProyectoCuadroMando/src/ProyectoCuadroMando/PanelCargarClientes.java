08fa1d4d23c7d2b5c7425fd7c42a3876927cad79
64500 <javier.roncero@gmail.com>
2013-03-17 14:41:44 +0100

    He posicionado los botones, la etiqueta y el cuadro de texto como se indica en el documento del proyecto. El cuadro de texto es "No Editable" y le he puesto el fondo de color blanco. El botón "Cancelar" borra lo que haya en el cuadro de texto y deshabilita el botón "Cargar".

--- ProyectoCuadroMando/src/ProyectoCuadroMando/PanelCargarClientes.java
+++ ProyectoCuadroMando/src/ProyectoCuadroMando/PanelCargarClientes.java
@@ -1,5 +1,6 @@
 package ProyectoCuadroMando;
 
+import java.awt.Color;
 import java.io.InputStream;
 import java.io.File;
 import java.awt.event.ActionEvent;
@@ -9,48 +10,73 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /*
  */
 public class PanelCargarClientes extends JPanel implements Action{
 
-    private JButton openButton;
+    private JButton botonSelector;
+    private JButton botonCancelar;
+    private JButton botonCargar;
     private JFileChooser selector;
+    private JTextField cuadroTextoRuta; 
    
     
     public PanelCargarClientes() {
         //Establecer tamaño y otros parámetros
         //Inicializar controles
-        JLabel etiqueta1 = new JLabel("Archivo de clientes :");
+        JLabel etiquetaArchivo = new JLabel("Archivo de clientes :");
         selector = new JFileChooser();
-        openButton = new JButton("Seleccionar archivo");
+        botonSelector = new JButton("Seleccionar archivo");
+        botonCancelar = new JButton("Cancelar");
+        botonCargar = new JButton("Cargar");
+        cuadroTextoRuta = new JTextField(40);
+        cuadroTextoRuta.setEditable(false);
+        cuadroTextoRuta.setBackground(Color.WHITE); 
+
             
         this.setLayout(null);
-        this.add(etiqueta1);
-        etiqueta1.setBounds(75,75,150,30);
-        this.add(openButton);
-        openButton.setBounds(450,75,150,30);
-        openButton.addActionListener(this);
-        
-        
-    }
+        this.add(etiquetaArchivo);
+        etiquetaArchivo.setBounds(75,75,120,30);
+        this.add(cuadroTextoRuta);
+        cuadroTextoRuta.setBounds(205,75,225,30);
 
+        this.add(botonSelector);
+        botonSelector.setBounds(450,75,150,30);
+        botonSelector.addActionListener(this);
+        this.add(botonCancelar);
+        botonCancelar.addActionListener(this);
+        botonCancelar.setBounds(350,225,150,30);
+        this.add(botonCargar);
+        botonCargar.setBounds(510,225,150,30);
+        botonCargar.setEnabled(false);
+       
+    }
 
     public void actionPerformed(ActionEvent e) {
         //Handle open button action.
-        if (e.getSource() == openButton) {
+        if (e.getSource() == botonSelector) {
             int returnVal = selector.showOpenDialog(PanelCargarClientes.this);
 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
                 File file = selector.getSelectedFile();
                 System.out.println(file.toString());
+                cuadroTextoRuta.setText(file.toString());
+                botonCargar.setEnabled(true);
+                
                 //This is where a real application would open the file.
                // log.append("Opening: " + file.getName() + "." + newline);
             } else {
                 //log.append("Open command cancelled by user." + newline);
             }
             //log.setCaretPosition(log.getDocument().getLength()); 
-    }}
+        } else if (e.getSource() == botonCancelar) {
+            
+            cuadroTextoRuta.setText("");
+            botonCargar.setEnabled(false);
+        }
+    }
 
     public Object getValue(String key) {
         throw new UnsupportedOperationException("Not supported yet.");
