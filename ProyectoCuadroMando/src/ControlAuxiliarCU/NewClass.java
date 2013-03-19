58fbcaac6bfe8199142e26b59be0c5cb65e31376
ivan <ivan@SATELLITE-R830>
2013-03-19 19:52:27 +0100

    Se crean los paquetes y se rembra la clase aplicacion

---
+++ ProyectoCuadroMando/src/ControlAuxiliarCU/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControlAuxiliarCU;
+
+/**
+ *
+ * @author ivan
+ */
+public class NewClass {
+    
+}
