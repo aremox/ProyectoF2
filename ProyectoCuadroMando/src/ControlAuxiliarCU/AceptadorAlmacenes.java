334af8654f8a289c4917826ed51d742570502fe8
64500 <javier.roncero@gmail.com>
2013-04-07 22:18:54 +0200

    PROYECTO FINAL. VERSION 1.14 ---------------------------------------------------------------------------------------- Se cambia el aceptador de Almacen

--- ProyectoCuadroMando/src/ControlAuxiliarCU/AceptadorAlmacenes.java
+++ ProyectoCuadroMando/src/ControlAuxiliarCU/AceptadorAlmacenes.java
@@ -19,35 +19,88 @@ public class AceptadorAlmacenes {
         textoError = "";
         resAceptar = true;
         
-        return (aceptarCodPostal(alm));
+        return (aceptarCalle(alm) && aceptarNumeroCalle(alm) && aceptarCodPostal(alm) && aceptarMunicipio(alm) && aceptarProvincia(alm));
     }
+
     
-    private boolean aceptarCodPostal(Almacen alm){
-        // Comprobamos que cod_postal es un número
-        try
-        {
-            int cod_postal = Integer.parseInt(alm.getCod_postal());
-            // Si es un numero vemos que este comprendido entre 01000 y 52999.
-            if ((cod_postal >= 01000) && (cod_postal <= 52999)){
-                resAceptar = true;
-            }
-            else
-            {
-                textoError = "El almacén "+ alm.getId_almacen()+" tiene un código postal erróneo ["+alm.getCod_postal()+"]";
+    private boolean aceptarCalle (Almacen alm){
+         if (resAceptar) {
+            if ("".equals(alm.getCalle())) {
+                textoError = "Almacén "+ alm.getId_almacen() +" con incoherencia de datos, el campo CALLE está vacío";
                 resAceptar = false;
             }
         }
-        catch(NumberFormatException nfe)
-        {
-            textoError = "Almacen con código postal no numérico";textoError = "El almacén "+ alm.getId_almacen()+" tiene un código postal no numérico ["+alm.getCod_postal()+"]";
-            resAceptar = false;
-        }
-        
         return resAceptar;
     }
 
+    private boolean aceptarNumeroCalle (Almacen alm){
+         if (resAceptar) {
+            if ("".equals(alm.getNumero())) {
+                textoError = "Almacén "+ alm.getId_almacen() +" con incoherencia de datos, el campo NÚMERO DE CALLE está vacío";
+                resAceptar = false;
+            }
+        }
+        return resAceptar;
+    }    
+
+    private boolean aceptarCodPostal (Almacen alm){
+         if (resAceptar) {
+            if ("".equals(alm.getCod_postal())) {
+                textoError = "Almacén "+ alm.getId_almacen() +" con incoherencia de datos, el campo CODIGO POSTAL está vacío";
+                resAceptar = false;
+            }
+        }
+        return resAceptar;
+    }       
+
+    private boolean aceptarMunicipio (Almacen alm){
+         if (resAceptar) {
+            if ("".equals(alm.getMunicipio())) {
+                textoError = "Almacén "+ alm.getId_almacen() +" con incoherencia de datos, el campo MUNICIPIO está vacío";
+                resAceptar = false;
+            }
+        }
+        return resAceptar;
+    }  
+    
+    private boolean aceptarProvincia (Almacen alm){
+         if (resAceptar) {
+            if ("".equals(alm.getProvincia())) {
+                textoError = "Almacén "+ alm.getId_almacen() +" con incoherencia de datos, el campo PROVINCIA está vacío";
+                resAceptar = false;
+            }
+        }
+        return resAceptar;
+    }     
+    
     
     public String getTextoError(){
         return textoError;
     }
+    
+    
+    /*private boolean aceptarCodPostal(Almacen alm){
+     * // Comprobamos que cod_postal es un número
+     * try
+     * {
+     * int cod_postal = Integer.parseInt(alm.getCod_postal());
+     * // Si es un numero vemos que este comprendido entre 01000 y 52999.
+     * if ((cod_postal >= 01000) && (cod_postal <= 52999)){
+     * resAceptar = true;
+     * }
+     * else
+     * {
+     * textoError = "El almacén "+ alm.getId_almacen()+" tiene un código postal erróneo ["+alm.getCod_postal()+"]";
+     * resAceptar = false;
+     * }
+     * }
+     * catch(NumberFormatException nfe)
+     * {
+     * textoError = "Almacen con código postal no numérico";textoError = "El almacén "+ alm.getId_almacen()+" tiene un código postal no numérico ["+alm.getCod_postal()+"]";
+     * resAceptar = false;
+     * }
+     * 
+     * return resAceptar;
+     * }*/
+
 }
--- ProyectoCuadroMando/src/EntidadesCU/Almacen.java
+++ ProyectoCuadroMando/src/EntidadesCU/Almacen.java
@@ -8,7 +8,7 @@ package EntidadesCU;
 
 public class Almacen {
 
-    private String id_almacenes;
+    private String id_almacen;
     private String calle;
     private String numero;
     private String cod_postal;
@@ -17,7 +17,7 @@ public class Almacen {
     private String provincia;
 
     public Almacen(String pid_almacenes, String pcalle, String pnumero, String pcod_postal, String ptelefono, String pmunicipio, String pprovincia) {
-        id_almacenes = pid_almacenes;
+        id_almacen = pid_almacenes;
         calle = pcalle;
         numero = pnumero;
         cod_postal = pcod_postal;
@@ -26,10 +26,25 @@ public class Almacen {
         provincia = pprovincia;
     }
     public String getId_almacen(){
-        return id_almacenes;
+        return id_almacen;
+    }
+    public String getCalle(){
+        return calle;
+    }
+    public String getNumero(){
+        return numero;
     }
     public String getCod_postal(){
         return cod_postal;
+    }
+    public String getTelefono(){
+        return telefono;
+    }
+    public String getMunicipio(){
+        return municipio;
+    }
+    public String getProvincia(){
+        return provincia;
     }
 
 }
